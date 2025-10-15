66cc193c999fae876114f9dcb74e624d8d25a8cc
Tanisha-221 <tanishajindal223@gmail.com>
2025-10-15 23:02:16 +0530

    changed trigger from feature to development

--- Infra-Terraform/module/acr/main.tf
+++ Infra-Terraform/module/acr/main.tf
@@ -1,7 +1,27 @@
 resource "azurerm_container_registry" "acr" {
-  name                = var.acr_name
-  resource_group_name = var.acr_resource_group_name
-  location            = var.acr_location
-  sku                 = "Standard"
-  admin_enabled       = var.acr_admin_enabled
+  name                     = var.acr_name
+  resource_group_name       = var.acr_resource_group_name
+  location                 = var.acr_location
+  sku                       = "Standard"
+  admin_enabled             = var.acr_admin_enabled
+}
+
+output "name" {
+  description = "The name of the Azure Container Registry"
+  value       = azurerm_container_registry.acr.name
+}
+
+output "login_server" {
+  description = "The login server URL for the ACR"
+  value       = azurerm_container_registry.acr.login_server
+}
+
+output "username" {
+  description = "The admin username for the ACR"
+  value       = azurerm_container_registry.acr.admin_username
+}
+
+output "password" {
+  description = "The admin password for the ACR"
+  value       = azurerm_container_registry.acr.admin_password
 }
