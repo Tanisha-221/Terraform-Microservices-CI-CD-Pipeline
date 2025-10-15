b578961887384a9bf50f4f4b0ba08a7ffbb7bee8
Tanisha-221 <tanishajindal223@gmail.com>
2025-10-15 12:09:21 +0530

    modified pipelines

--- Infra-Terraform/output.tf
+++ Infra-Terraform/output.tf
@@ -7,8 +7,3 @@ output "acr_resource_group_name" {
   description = "The resource group name for the ACR"
   value       = module.acr.resource_group_name
 }
-
-output "app_service_publish_profile" {
-  value     = azurerm_linux_web_app.awa.publishing_profile
-  sensitive = true
-}
